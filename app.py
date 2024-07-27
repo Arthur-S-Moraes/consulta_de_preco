bd641f04115fc6f31c414779ef607662640fc823
Arthur Moraes <143559418+Arthur-S-Moraes@users.noreply.github.com>
2024-07-27 16:37:36 -0300

    adição de uma interface gráfica

---
+++ app.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+import openpyxl
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as condicao_esperada
+from selenium.common.exceptions import *
+
+def projeto_monitorar_preco(window,site):
+    def iniciar_driver():
+        print('iniciando o programa, aguarde alguns instantes')
+        chrome_option = Options()
+        arguments = ['--lang=en-US', '--window-size=1080,1080', '--force-dark-mode', '--log-level=3']
+        for argument in arguments:
+            chrome_option.add_argument(argument)
+
+
+        driver = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()), options=chrome_option)
+        
+        wait = WebDriverWait(
+            driver,
+            10,
+            poll_frequency=1,
+            ignored_exceptions=[
+                NoSuchElementException,
+                ElementNotVisibleException,
+                ElementNotSelectableException
+            ]
+        )
+        return driver, wait
+    def buscar_produto():
+        driver, wait = iniciar_driver()
+        print('iniciando o navegador')
+        try:
+            driver.get(site)
+            print('buscando o valor do produto selecionado')
+            produto = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH, "//h1[@class='sc-58b2114e-6 brTtKt']"))).text
+            valor = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH, "//h4[@class='sc-5492faee-2 ipHrwP finalPrice']"))).text
+            valor = valor.split(' ')[1]
+        except:
+            print('''tivemos algum erro e não foi possivel encontrar o valor do item selecionado
+                use uma URL com o produto que deseja buscar o valor''')
+        return(produto, valor, site)
+    def criar_editar_planilha():
+        produto, valor, site = buscar_produto()
+
+        try:
+            workbook = openpyxl.load_workbook('produtos.xlsx')
+            sheet_produtos = workbook['Produtos']
+            print('carregando e atualizando a planilha salva')
+        except:
+            workbook = openpyxl.Workbook()
+            print('criando nova planilha')
+            workbook['Sheet'].title = 'Produtos'
+            sheet_produtos = workbook['Produtos']
+            sheet_produtos.append(['Produto', 'Data atual', 'Valor', 'Link'])
+        data_atual = datetime.now()
+        data_formatada = data_atual.strftime('%d/%m/%Y %H:%M')
+
+        for linha in sheet_produtos.iter_rows(min_row=2, max_row=100,min_col=1, max_col=4):
+            if linha[0].value == produto and linha[3].value == site:
+                linha[1].value = data_formatada
+                linha[2].value = valor
+                break
+            elif linha[0].value == None:
+                linha[0].value = produto
+                linha[1].value = data_formatada
+                linha[2].value = valor
+                linha[3].value = site
+                break
+
+        workbook.save('produtos.xlsx')
+    criar_editar_planilha()
+    window.write_event_value('planilha_atualizada','Planilha salva com sucesso')
---
+++ interface.py
@@ -0,0 +1,58 @@
+from app import projeto_monitorar_preco
+import PySimpleGUI as sg
+from threading import Thread
+import schedule
+from time import sleep
+
+def executar_schedule(intervalo, window, site):
+    schedule.every(intervalo).minutes.do(projeto_monitorar_preco, window, site)
+    while True:
+        schedule.run_pending()
+        sleep(1)
+sg.theme('black')
+
+layout = [
+    [sg.Text('digite o URL de um produto do site da Kabum')],
+    [sg.Input(key='site')],
+    [sg.Text('a cada quantos minutos você deseja verificar o site')],
+    [sg.Input(key='tempo')],
+    [sg.Text('Iniciar a primeira vez imediatamente?')],
+    [sg.Checkbox('Sim',key='iniciar_imediatamente')],
+    [sg.Button('Buscar', key='buscar')],
+    [sg.Button('Encerrar programa', key='encerrar',  button_color='RED')],
+    [sg.Output(size=(50,10))],
+]
+
+window = sg.Window('busca de preço', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    elif event == 'buscar':
+        site = values['site']
+        try:
+            intervalo = float(values['tempo'])
+        except:
+            print("pro favor, digite apenas numeros")
+            continue
+
+        running = True
+        if values['iniciar_imediatamente'] == True:
+            thread_bot = Thread(target=projeto_monitorar_preco,args=(window, site), daemon=True)
+            thread_bot.start()
+        else:
+            print(f'programa iniciando em {values["tempo"]} minutos, por favor aguarde')
+            thread_bot = Thread(target=executar_schedule,args=(intervalo, window, site), daemon=True)
+            thread_bot.start()
+            window['buscar'].update(disabled=True)
+        
+    elif event == 'planilha_atualizada':
+        # thread_bot.join()
+        print(values['planilha_atualizada'])
+        window['buscar'].update(disabled=False)
+        print(f'próxima atualização em {values["tempo"]} minutos')
+
+    elif event == 'encerrar':
+        break
